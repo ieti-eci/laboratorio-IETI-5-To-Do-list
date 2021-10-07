e495350d578c245cf13441c0641a2a524efb7a4a
JamesTorres99 <69390068+JamesTorres99@users.noreply.github.com>
2021-10-07 01:40:39 -0500

    Add onTaskChange, useState and handleTaskChange react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import { TaskItem } from "./TaskItem";
+import {useState} from "react";
+
 function App() {
   //business logic
+    const [tasks, setTasks]  = useState ([
+        {
+            isCompleted:true,
+            name: "Learn React on Class",
+        },
+        {
+            isCompleted:true,
+            name: "Learn Hocks",
+        },
+        {
+            isCompleted:true,
+            name: "Keep on Keeping on",
+        }
+    ]);
+
+    const handleTaskChange = (index) => () =>{
+        console.log("changed! "+ index);
+        const arr = [...tasks];
+        arr[index].isCompleted = !arr[index].isCompleted
+        setTasks(arr);
+    }
   return ( <main>
               <form>
                   <input type="text" placeholder="Task name"/>
                   <button>Create Task</button>
               </form>
               <ul>
-                  <TaskItem isChecked={true} taskName="Learn React" />
-                  <TaskItem isChecked={false} taskName="Learn Hocks" />
-                  <TaskItem isChecked={true} taskName="Keep on Keeping on" />
+                  {tasks.map((task, index) => {
+                      return (
+                          <TaskItem
+                              isChecked={task.isCompleted}
+                              taskName={task.name}
+                              onTaskChange={handleTaskChange(index)}
+                          />
+                      );
+                  })}
               </ul>
       </main>
   );
--- src/TaskItem.js
+++ src/TaskItem.js
@@ -1,11 +1,11 @@
-export const TaskItem = ({isChecked,taskName}) => {
+export const TaskItem = ({isChecked,taskName,onTaskChange}) => {
 
     const styleOfTheComponent = {
         textDecoration: isChecked ? "line-through" : "",};
 
     return (
         <li>
-            <input checked={isChecked} type="checkbox"/>
+            <input onChange={onTaskChange} checked={isChecked} type="checkbox"/>
             <span style={styleOfTheComponent }>{taskName}</span>
         </li>
     );
