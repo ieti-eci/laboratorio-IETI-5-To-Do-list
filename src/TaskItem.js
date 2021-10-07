de6d155ee51e27c22aabab49c385f7134fd19ccb
JamesTorres99 <69390068+JamesTorres99@users.noreply.github.com>
2021-10-07 01:20:00 -0500

    Add TaskItem.js and initial hooks react

---
+++ src/TaskItem.js
@@ -0,0 +1,12 @@
+export const TaskItem = ({isChecked,taskName}) => {
+
+    const styleOfTheComponent = {
+        textDecoration: isChecked ? "line-through" : "",};
+
+    return (
+        <li>
+            <input checked={isChecked} type="checkbox"/>
+            <span style={styleOfTheComponent }>{taskName}</span>
+        </li>
+    );
+};
